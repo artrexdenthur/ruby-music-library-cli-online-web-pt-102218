e308bc2c938dbd95298ff9269f6c3887114e2ede
Paul Ashour <paul.ashour@gmail.com>
2018-12-16 20:29:57 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -37,7 +37,7 @@ class MusicLibraryController
   end
   
   def list_artists
-    binding.pry
+    # binding.pry
     Artist.all.sort_by { |artist| artist.name }.each_with_index do |artist, i|
       puts "#{i+1}. #{artist.name}"
     end
